a303aef2a065f1dea6522c5de4dd2239e953f92e
Ciprian Eniță <ciprian_enita@yahoo.com>
2025-08-21 13:52:49 +0300

    chore: improve fee percent display

--- src/FeeStructureUpdated.ts
+++ src/FeeStructureUpdated.ts
@@ -6,7 +6,7 @@ Management.FeeStructureUpdated.handler(async ({ event, context }) => {
     id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
     chainId: event.chainId.toString(),
     timestamp: BigInt(event.block.timestamp),
-    fee: `${formatUnits(event.params.feeStructure[0], 18)} %`,
+    fee: `${formatUnits(event.params.feeStructure[0], 18)}%`,
     period: event.params.feeStructure[1],
     minimumAmount: event.params.feeStructure[2],
     hash: event.block.hash,
